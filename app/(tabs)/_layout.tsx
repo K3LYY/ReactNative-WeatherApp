6bfacd0a670eb67b233d6a0844871e32c9811251
K3LYY <maliszek.j@gmail.com>
2026-03-19 12:53:19 +0100

    changed header and navbar styles to match the rest of the app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,40 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import { Tabs } from "expo-router";
+import { Styles } from "@/styles/Styles";
+import { useColorScheme } from "react-native";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const themeTextStyle =
+    colorScheme === "light" ? Styles.lightTextColor : Styles.darkTextColor;
+  const secondThemeContainerStyle =
+    colorScheme === "light"
+      ? Styles.secondLightContainerColor
+      : Styles.secondDarkContainerColor;
+  const themeContainerStyle =
+    colorScheme === "light"
+      ? Styles.lightContainerColor
+      : Styles.darkContainerColor;
+  const themeBorderStyle =
+    colorScheme === "light" ? Styles.lightBorder : Styles.darkBorder;
+
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: secondThemeContainerStyle.backgroundColor,
+        },
+        headerTintColor: themeTextStyle.color,
+        tabBarStyle: {
+          backgroundColor: secondThemeContainerStyle.backgroundColor,
+          borderTopColor: themeBorderStyle.borderColor,
+          borderTopWidth: 1,
+        },
+        tabBarActiveTintColor: themeTextStyle.color,
+        tabBarInactiveTintColor: colorScheme === "light" ? "#666" : "#ccc",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
